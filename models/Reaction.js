75e1505eac65ae51b38c923c7f2073a5f2186525
Miles Wildmore <mileswildmore18@gmail.com>
2024-05-22 17:52:06 -0400

    Added the properties to the Reaction Schema model js file. Next will be to add API properties to the routes.

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,2 +1,31 @@
 const mongoose = require('mongoose');
-const {Schema, Types } = mongoose;
+const { Schema, Types } = mongoose;
+
+//Defining the Reaction schema
+const reactionSchema = new Schema({
+    reactionId: {
+        type: Types.ObjectId,
+        default: new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => new DataTransfer(timestamp).toISOString()
+    }
+}, {//Ensuring getters are applied when converting the document to JSON
+    toJSON: {
+        getters: true
+    },
+    id: false
+}); //Disables the virtual 'id' field since '_id' is present
+
+module.exports = reactionSchema;
