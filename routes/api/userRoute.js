695729db49fb40e3c8d53089b304a3ca3e8ef8eb
Miles Wildmore <mileswildmore18@gmail.com>
2024-05-22 19:14:25 -0400

    Adding Pseudocodes in the user Route to help understand what is happening with the codes.

--- routes/api/userRoute.js
+++ routes/api/userRoute.js
@@ -6,6 +6,7 @@ const User = require('../../models/User');
 //GET all users
 router.get('/', async (req, res) => {
     try {
+        //Gathering up information on all users
         const users = await User.find();
         res.json(users);
     } catch (err) {
@@ -16,6 +17,7 @@ router.get('/', async (req, res) => {
 //GET a single user by its _id and populated thought and friend data
 router.get('/:id', async (req, res) => {
     try {
+        //Finding user by id, their thoughts and friends
         const user = await User.findById(req.params.id)
             .populate('thoughts')
             .populate('friends');
@@ -28,12 +30,14 @@ router.get('/:id', async (req, res) => {
 
 //Using POST to add a new user
 router.post('/', async (req, res) => {
+    //Adding user by id and email
     const user = new User({
         username: req.body.username,
         email: req.body.email
     });
 
     try {
+        //Saving the new user and their email and updating the user documents
         const newUser = await user.save();
         res.status(201).json(newUser);
     } catch (err) {
@@ -44,6 +48,7 @@ router.post('/', async (req, res) => {
 // Using PUT to update a user by its _id
 router.put('/:id', async (req, res) =>{
     try {
+        //finding the user by id and email
         const user = await User.findById(req.params.id);
         if (!user) return res.status(404).json({ message: 'User not found'});
 
@@ -53,7 +58,7 @@ router.put('/:id', async (req, res) =>{
         if (req.body.email != null) {
             user.email = req.body.email;
         }
-
+        //Saving the user and their email and updating the user documents
         const updatedUser = await user.save();
         res.json(updatedUser);
     } catch (err) {
@@ -64,9 +69,11 @@ router.put('/:id', async (req, res) =>{
 //Using DELETE to remove a user by its _id
 router.delete('/:id', async (req,res) =>{
     try {
+        //finding the user by id
         const user = await User.findById(req.params.id);
         if (!user) return res.status(404).json({ message: 'User not found'});
 
+        //deleting the user
         await user.remove();
         res.json({message: 'User deleted'});
     } catch (err) {
